3e49aba165d1de4259a1302acb83a0efc70552ee
Luis Rene Mas Mas <01luisrene@gmail.com>
2016-04-24 12:53:33 -0500

    subiendo modificaciones para theme up
    subiendo al repo

---
+++ content/themes/up/assets/js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+function search(){
+	var searchField =  $('#js_campo_search').ghostHunter({
+    results             : '#js_list_results',
+    info_template   : "<p class='number-posts'>Number of posts found: {{amount}}</p>",
+    result_template : "<li><a href='{{link}}'><p><h2><i class='material-icons md-18'>radio_button_checked</i>  {{title}}</h2></p></a></li>",
+    onKeyUp             : true,
+    displaySearchInfo   : true
+  });
+  $('#js_campo_search').on('keyup', function() {
+  	var $caja_buscar = $('#js_campo_search').val().length;
+	  if($caja_buscar > 0){
+  		$('#js_box_results').css({
+  			display: 'block'
+  		}).addClass('animated zoomIn');
+	  }
+	  if($caja_buscar == 0){
+	  	$('#js_box_results').css({
+				display: 'none'
+			});
+	  }
+  });
+}
+function closeSearch(){
+	$('#js_box_results_close').on('click', function(event) {
+		var $caja_buscar = $('#js_campo_search');
+		setTimeout(function(){ $caja_buscar.val(''); }, 2000);
+		$('#js_box_results').css({
+				display: 'none'
+		});
+	});
+}
+$(document).ready(function() {
+	search();
+	closeSearch()
+});
